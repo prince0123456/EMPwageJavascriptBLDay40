8d5803a040cc3e4794fac879cbe7b1b296b6ff6f
Prince Gupta <princegupta.pg46@gmail.com>
2023-06-14 08:29:00 +0530

    calculat daily wage

--- empwage.js
+++ empwage.js
@@ -1,7 +1,24 @@
-console.log("Welcome To Employe wage program using JS");
-let present=1;
-let empInput=Math.floor((Math.random()*10)%2);
-if(empInput==present)
-    console.log("Employee is present")
-else
-    console.log("Employee is absent");
+console.log("Welcome To Employee wage program using JS");
+let IS_FULL_TIME=1;
+let IS_PART_TIME=2;
+let EMP_RATE_PER_HR=20;
+let empHrs=0;
+let empWage=0;
+let empInput=Math.floor((Math.random()*10)%3);
+
+switch(empInput)
+{
+    case IS_FULL_TIME:
+        console.log("Employee present full time");
+        empHrs=8;
+        break;
+    case IS_PART_TIME:
+        console.log("Employee present part time");
+        empHrs=4;
+        break;
+    default:
+        console.log("Employee is absent");
+        empHrs=0;
+}
+empWage=EMP_RATE_PER_HR*empHrs;
+console.log("Employee wage is:{0}",empWage);
