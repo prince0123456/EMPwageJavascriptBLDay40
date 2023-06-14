bab042c0e8266bd8af4c4f588fde68dc23eaef17
Prince Gupta <princegupta.pg46@gmail.com>
2023-06-14 08:34:25 +0530

    calculat monthly wage using function

--- empwage.js
+++ empwage.js
@@ -2,23 +2,34 @@ console.log("Welcome To Employee wage program using JS");
 let IS_FULL_TIME=1;
 let IS_PART_TIME=2;
 let EMP_RATE_PER_HR=20;
-let empHrs=0;
-let empWage=0;
+const MAX_WORKING_DAYS=20;
+let empHrs=0,totalEmpHrs=0;
+let empWage=0,totalEmpWage=0;
 let empInput=Math.floor((Math.random()*10)%3);
 
-switch(empInput)
-{
-    case IS_FULL_TIME:
-        console.log("Employee present full time");
-        empHrs=8;
-        break;
-    case IS_PART_TIME:
-        console.log("Employee present part time");
-        empHrs=4;
-        break;
-    default:
-        console.log("Employee is absent");
-        empHrs=0;
+function GetEmpHours(empInput)
+{ 
+    switch(empInput)
+    {
+        case IS_FULL_TIME:
+            console.log("Employee present full time");
+            return empHrs=8;
+            
+        case IS_PART_TIME:
+            console.log("Employee present part time");
+            return empHrs=4;
+            
+        default:
+            console.log("Employee is absent");
+            return empHrs=0;
+    }
 }
-empWage=EMP_RATE_PER_HR*empHrs;
-console.log("Employee wage is:{0}",empWage);
+for(var day=0;day<=MAX_WORKING_DAYS;day++)
+{
+    let empInput=Math.floor((Math.random()*10)%3);
+    empWage=EMP_RATE_PER_HR*GetEmpHours(empInput);
+    console.log("day "+day+" wage is "+(empHrs*EMP_RATE_PER_HR))
+    totalEmpHrs+=empHrs;
+}
+totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
+console.log("Total Employee wage is:",totalEmpWage);
