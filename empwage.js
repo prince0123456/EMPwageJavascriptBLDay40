e4d781868ed3f9e392413d57d0034ae549e47e27
Prince Gupta <princegupta.pg46@gmail.com>
2023-06-14 08:22:19 +0530

    Chech Emp Absent Or present

---
+++ empwage.js
@@ -0,0 +1,7 @@
+console.log("Welcome To Employe wage program using JS");
+let present=1;
+let empInput=Math.floor((Math.random()*10)%2);
+if(empInput==present)
+    console.log("Employee is present")
+else
+    console.log("Employee is absent");
