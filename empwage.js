1ea959bd9d899bd5d2bc0f54ece20bf942a9f557
Prince Gupta <34147489+prince0123456@users.noreply.github.com>
2023-06-18 19:47:49 +0530

    Update empwage.js
    functions

--- empwage.js
+++ empwage.js
@@ -6,19 +6,22 @@ let empHrs=0;
 let empWage=0;
 let empInput=Math.floor((Math.random()*10)%3);
 
-switch(empInput)
-{
-    case IS_FULL_TIME:
-        console.log("Employee present full time");
-        empHrs=8;
-        break;
-    case IS_PART_TIME:
-        console.log("Employee present part time");
-        empHrs=4;
-        break;
-    default:
-        console.log("Employee is absent");
-        empHrs=0;
+function GetEmpHours(empInput)
+{ 
+    switch(empInput)
+    {
+        case IS_FULL_TIME:
+            console.log("Employee present full time");
+            return empHrs=8;
+            
+        case IS_PART_TIME:
+            console.log("Employee present part time");
+            return empHrs=4;
+            
+        default:
+            console.log("Employee is absent");
+            return empHrs=0;
+    }
 }
-empWage=EMP_RATE_PER_HR*empHrs;
-console.log("Employee wage is:{0}",empWage);
+empWage=EMP_RATE_PER_HR*GetEmpHours(empInput);
+console.log("By function Employee wage is:",empWage);
